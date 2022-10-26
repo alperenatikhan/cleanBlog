1cb7151853c3de015f326520213e3cfc69dc5efe
alp <alperenmyung@gmail.com>
2022-10-26 21:11:09 +0200

    ejs branch

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express= require('express');
 const app = express()
+const ejs= require('ejs')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
 const port= 3000
-
-
-
 
 app.get('/', (req,res) =>{
 
-    const blog = { id: 1, title: "Blog title", description: "Blog description" };
-    res.send(blog)
+ res.render('index')
 
 })
 
-app.listen(port, ()=> console.log('it started'))
+app.get('/about', (req,res) =>{
+
+    res.render('about')
+   
+   })
+
+app.get('/contact', (req,res) =>{
+
+    res.render('contact')
+   
+   })
+
+   app.get('/video-page', (req,res) =>{
+
+    res.render('video-page')
+   
+   })
+
+app.listen(port, ()=> console.log(`it started ${port}`))
